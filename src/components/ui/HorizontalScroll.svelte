876536659707c9e772b03931d9065b409b6c74d4
naitik <devloper2345@gmail.com>
2026-07-01 18:28:07 +0530

    UI Improvements

--- src/components/ui/HorizontalScroll.svelte
+++ src/components/ui/HorizontalScroll.svelte
@@ -2,10 +2,12 @@
     let { title, children } = $props();
 </script>
 
-<h2 class="text-4xl font-extrabold text-white font-switzer mb-4">
+<h2 class="text-5xl font-extrabold text-white font-switzer mb-4">
     {title}
 </h2>
-<div class="relative flex gap-10 overflow-x-auto scrollbar-hide px-2">
+<div
+    class="relative flex gap-10 justify-evenly overflow-x-auto scrollbar-hide px-2 w-full"
+>
     {@render children()}
 </div>
 
